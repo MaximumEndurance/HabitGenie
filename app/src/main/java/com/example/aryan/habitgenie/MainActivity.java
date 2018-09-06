da89145b66ecefffbaa413f06f6ef0bfceec3a3b
Siddharth Dangwal <maximumendurance1965@gmail.com>
2018-09-06 16:19:17 +0530

    Adds the plus icon to add habits

--- app/src/main/java/com/example/aryan/habitgenie/MainActivity.java
+++ app/src/main/java/com/example/aryan/habitgenie/MainActivity.java
@@ -22,9 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        String currentHabits[] = readFromTable();
+//        String currentHabits[] = readFromTable();
         String currentHabits[] = { "Habit1", "Habit2" };
         ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, R.layout.content_main,
                 currentHabits);
--- app/src/main/java/com/example/aryan/habitgenie/helper/SQLHelper.java
+++ app/src/main/java/com/example/aryan/habitgenie/helper/SQLHelper.java
@@ -3,12 +3,12 @@ package com.example.aryan.habitgenie.helper;
 import com.example.aryan.habitgenie.model.HabitEntryContract.HabitEntry;
 
 public class SQLHelper {
-    private static final String CREATE_HABIT_TABLE = "CREATE TABLE " + HabitEntry.TABLE_NAME +
+    public static final String CREATE_HABIT_TABLE = "CREATE TABLE " + HabitEntry.TABLE_NAME +
             " (" +     HabitEntry._ID + " INTEGER PRIMARY KEY," +
             HabitEntry.COLUMN_NAME_TITLE + " TEXT," +
             HabitEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
 
 
-    private static final String DELETE_HABIT_TABLE =
+    public static final String DELETE_HABIT_TABLE =
             "DROP TABLE IF EXISTS " + HabitEntry.TABLE_NAME;
 }
